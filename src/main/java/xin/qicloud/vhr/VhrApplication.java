e1fcb2c94e2d52b9566a1799ed9a073a6d88f572
qiyb <qiyunbo7@126.com>
2019-08-27 15:05:02 +0800

    微人事系统初始化

---
+++ src/main/java/xin/qicloud/vhr/VhrApplication.java
@@ -0,0 +1,13 @@
+package xin.qicloud.vhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhrApplication.class, args);
+    }
+
+}
